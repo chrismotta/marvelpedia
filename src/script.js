aa94f4cf43bc6db5cfbd0f6b986a9066a0b53e98
Christian Motta <cm@castinellc.com>
2020-12-23 13:34:20 -0300

    likes

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@
 
 const characters = [
   {
+    _id: 1,
     name: "Spiderman",
     realName: "Peter Parker",
     creationYear: "1962",
@@ -21,7 +22,8 @@ const characters = [
       },
       {
         user: "Leon",
-        comment: "Me parece una excelente publicacion, sigan asi desarrolladores!",
+        comment:
+          "Me parece una excelente publicacion, sigan asi desarrolladores!",
       },
       {
         user: "Chris",
@@ -30,6 +32,7 @@ const characters = [
     ],
   },
   {
+    _id: 2,
     name: "Captain Marvel",
     realName: "Carol Danvers",
     creationYear: "1986",
@@ -40,15 +43,18 @@ const characters = [
     comments: [],
   },
   {
+    _id: 3,
     name: "Hulk",
-    realName:"Bruce Banner",
-    creationYear:"1962",
+    realName: "Bruce Banner",
+    creationYear: "1962",
     image: "https://i.blogs.es/2f9456/inmortal-hulk/450_1000.jpg",
     info:
       "In his comic book appearances, the character is both the Hulk, a green-skinned, hulking and muscular humanoid possessing a vast degree of physical strength, and his alter ego Dr. Robert Bruce Banner, a physically weak, socially withdrawn, and emotionally reserved physicist.",
     comments: [],
   },
 ];
+
+let likedCharacters = [];
 
 // const card = "hola " + character.name + ", como estas?";//concat string
 // const card = `hola ${character.name}, como estas?`;//template literal
@@ -57,63 +63,89 @@ const characters = [
 // const card = document.createElement("div");
 // card.classList.add("card");
 
-let cards = "";
-// cada personaje
-for (i = 0; i < characters.length; i++) {
-  cards += `
-    <div class="card">
-      <div class="title">
-      <div>Fecha de creación: ${characters[i].creationYear}</div>
-        <span class="text">${characters[i].name}</span>
-        <span class="real-name">${characters[i].realName}<span>
-        <span class="menu">
-          <i class="fas fa-ellipsis-v"></i>
-        </span>
-      </div>
-      <div class="card-body">
-        <div class="image">
-          <img
-            src="${characters[i].image}"
-            alt=""
-          />
-        </div>
-        <div class="card-info">
-          <div class="media">
-            <span class="icon">
-              <i class="far fa-heart"></i>
-            </span>
-            <span class="icon">
-              <i class="far fa-comment"></i>
-            </span>
-          </div>
-          <div class="description">
-            ${characters[i].info}
-          </div>
-          <div class="comments-container">`;
-  // cada comentario
-  const arrayComments = characters[i].comments;
-  for (g = 0; g < arrayComments.length; g++) {
+function renderCharacters() {
+  let cards = "";
+  // cada personaje
+  for (i = 0; i < characters.length; i++) {
+    let likedClass;
+    // si el id esta dentro de likedCharacters el icono va a ser lleno, sino vacio
+    if (likedCharacters.includes(characters[i]._id)) {
+      likedClass = "fas fa-heart";
+    } else {
+      likedClass = "far fa-heart";
+    }
+
     cards += `
-            <div class="comment">
-              <span class="user"><a href="">${arrayComments[g].user}</a></span>
-              <span class="text">${arrayComments[g].comment}</span>
+      <div class="card">
+        <div class="title">
+        <div>Fecha de creación: ${characters[i].creationYear}</div>
+          <span class="text">${characters[i].name}</span>
+          <span class="real-name">${characters[i].realName}<span>
+          <span class="menu">
+            <i class="fas fa-ellipsis-v"></i>
+          </span>
+        </div>
+        <div class="card-body">
+          <div class="image">
+            <img
+              src="${characters[i].image}"
+              alt=""
+            />
+          </div>
+          <div class="card-info">
+            <div class="media">
+              <button class="icon" onclick="like(${characters[i]._id})" >
+                <i class="${likedClass}"></i>
+              </button>
+              <button class="icon">
+                <i class="far fa-comment"></i>
+              </button>
             </div>
-            `;
+            <div class="description">
+              ${characters[i].info}
+            </div>
+            <div class="comments-container">`;
+    // cada comentario
+    const arrayComments = characters[i].comments;
+    for (g = 0; g < arrayComments.length; g++) {
+      cards += `
+              <div class="comment">
+                <span class="user"><a href="">${arrayComments[g].user}</a></span>
+                <span class="text">${arrayComments[g].comment}</span>
+              </div>
+              `;
+    }
+
+    cards += `
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
   }
 
-  cards += `
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
+  const cardContainer = document.querySelector("#cards-container");
+  // Añadir elementos estilo string concat (vieja usanza)
+  cardContainer.innerHTML = cards;
+  // const card2 = card;
+  // cardContainer.appendChild(card);
 }
 
-const cardContainer = document.querySelector("#cards-container");
-// Añadir elementos estilo string concat (vieja usanza)
-cardContainer.innerHTML = cards;
-// const card2 = card;
-// cardContainer.appendChild(card);
+function like(characterID) {
+  // console.log("hay un like para el character", characterID);
+
+  // si el id esta en la lista hay que sacarlo, sino agregarlo
+  // el indexOf nos devuelve la posicion del elemento en la lista
+  // si el elemento no esta indexOf devuelve -1
+  let characterIndex = likedCharacters.indexOf(characterID);
+  if (characterIndex === -1) {
+    likedCharacters.push(characterID);
+  } else {
+    likedCharacters.splice(characterIndex, 1);
+  }
+  console.log(likedCharacters);
+  renderCharacters();
+}
 
 // suma de strings
 // let valor = "hola ";
@@ -129,3 +161,10 @@ cardContainer.innerHTML = cards;
 // for (i = 0; i < characters.length; i++) {
 //   console.log("character", characters[i].name, characters[i].age);
 // }
+
+renderCharacters();
+
+// tarea
+// agregar contador de likes totales del usuario usando array.length
+// hacerlo con position fixed para que sea flotante
+// convertirlo en una funcion render que se ejecute con cada cambio
